45cd2c274e7243ede5e559853d20f0e1b2278754
9alecj <amw919@gmail.com>
2014-12-17 02:20:13 -0500

    control and particles
    testing commit 1 :  12/17

---
+++ Project/Assets/controls.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controls : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+						Vector3 position = this.transform.position;
+						position.x--;
+						this.transform.position = position;
+				}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			Vector3 position = this.transform.position;
+			position.x++;
+			this.transform.position = position;
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			Vector3 position = this.transform.position;
+			position.z++;
+			this.transform.position = position;
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			Vector3 position = this.transform.position;
+			position.z--;
+			this.transform.position = position;
+		}
+	
+	}
+}
